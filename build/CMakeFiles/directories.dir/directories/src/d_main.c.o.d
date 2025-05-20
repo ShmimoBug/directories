f35bbc68622aa441effdbbfcd6686c8e45ddd9d0
Matthew Santana <45676223+ShmimoBug@users.noreply.github.com>
2025-05-19 21:13:27 -0700

    Create .gitignore

--- build/CMakeFiles/directories.dir/directories/src/d_main.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/directories.dir/directories/src/d_main.c.o: \
- /home/matthew/Programming/C/directories/directories/src/d_main.c \
- /usr/include/stdc-predef.h \
- /home/matthew/Programming/C/directories/directories/include/d_main.h \
- /home/matthew/Programming/C/directories/directories/include/choices.h \
- /home/matthew/Programming/C/directories/directories/include/file_system.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h
--- directories/include/d_main.h
+++
@@ -1,6 +0,0 @@
-#ifndef D_MAIN
-#define D_MAIN
-
-int DirectoriesMain(int argc, char* argv[]);
-
-#endif // D_MAIN
